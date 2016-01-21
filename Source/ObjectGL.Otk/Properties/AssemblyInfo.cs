e37614364166df7b739c7d3be897272011744515
Daniil Rodin <furidan@gmail.com>
2016-01-21 17:13:33 +0200

    Renamed GL4 to Otk

--- Source/ObjectGL.Otk/Properties/AssemblyInfo.cs
+++ Source/ObjectGL.Otk/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ObjectGL.GL4")]
+[assembly: AssemblyTitle("ObjectGL.Otk")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ObjectGL.GL4")]
+[assembly: AssemblyProduct("ObjectGL.Otk")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
